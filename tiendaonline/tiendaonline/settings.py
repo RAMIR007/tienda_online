596584b173a4af779414857decb57e9d75fa321a
RAMIR007 <ramiroxavier07@gmail.com>
2025-09-04 21:48:44 -0400

    Crear modelo básico Producto en la app productos

--- tiendaonline/tiendaonline/settings.py
+++ tiendaonline/tiendaonline/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Havana'
 
 USE_I18N = True
 
@@ -124,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # Puerto por defecto de React
+]
