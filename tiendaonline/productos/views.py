c039ea76919c35ffd90460f438c695bc8fc73833
RAMIR007 <ramiroxavier07@gmail.com>
2025-09-04 21:53:11 -0400

    Creación serializer y vistas API para Productos

--- tiendaonline/productos/views.py
+++ tiendaonline/productos/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Producto
+from .serializers import ProductoSerializer
 
-# Create your views here.
+class ProductoListCreate(generics.ListCreateAPIView):
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
